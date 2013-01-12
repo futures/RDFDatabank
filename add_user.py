78573cd2ba18061b932e40a47d5bf394da5fa58d
Chris Beer <cabeer@stanford.edu>
2013-01-12 11:36:17 -0800

    update add_user.py to take the configuration file as the first argument..

--- add_user.py
+++ add_user.py
@@ -6,11 +6,16 @@ import sys, os
 
 if __name__ == "__main__":
     #Initialize sqlalchemy
-    f = '/var/lib/databank/production.ini' 
+    f = sys.argv[1]
     if not os.path.exists(f):
         print "Config file not found"
         sys.exit()
-    c = ConfigParser.ConfigParser()
+    defaults = {
+        'here':   os.path.dirname(os.path.abspath(f)),
+        '__file__':  os.path.abspath(f)
+    }
+
+    c = ConfigParser.ConfigParser(defaults)
     c.read(f)
     if not 'app:main' in c.sections():
         print "Section app:main not found in config file"
@@ -20,9 +25,9 @@ if __name__ == "__main__":
     init_model(engine)
 
     #add user
-    username = sys.argv[1]
-    password = sys.argv[2]
-    email = sys.argv[3]
+    username = sys.argv[2]
+    password = sys.argv[3]
+    email = sys.argv[4]
     user_details = {
         'username':u'%s'%username,
         'password':u"%s"%password,
